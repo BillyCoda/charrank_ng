1b07ca2f441ca434a78f69affd33a17409dc3f15
William Newman <billycoda@gmail.com>
2020-03-10 22:05:05 -0600

    Basic splash page impl

--- src/app/entry/splash/splash.component.ts
+++ src/app/entry/splash/splash.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./splash.component.scss']
 })
 export class SplashComponent implements OnInit {
+  showLogin = true;
+  showSignup = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleLogin() {
+    this.showLogin = true;
+    this.showSignup = false;
+  }
+
+  toggleSignup() {
+    this.showLogin = false;
+    this.showSignup = true;
   }
 
 }
